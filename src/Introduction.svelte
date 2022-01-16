87b7da09b26ee2000d600f0c6d8772d382405131
ckolin <25738463+ckolin@users.noreply.github.com>
2022-01-16 21:39:36 +0100

    Add example image

--- src/Introduction.svelte
+++ src/Introduction.svelte
@@ -1,16 +1,21 @@
-<div>
-    <h1>Marschzeit&shy;tabelle<br />leicht gemacht.</h1>
-    <p class="subtitle">Wir übertragen deine Route automatisch in eine Marschzeittabelle, damit du dich auf die wichtigen Teile der Planung konzentrieren kannst.</p>
-    <h2>1. Route planen</h2>
-    <p>Route einzeichnen, Wegpunkte setzen - das kennst du bereits.</p>
-    <h2>2. Exportieren und hochladen</h2>
-    <p>In wenigen Klicks erledigt.</p>
-    <h2>3. Voilà!</h2>
-    <p>Schon hast du deine Marschzeittabelle.<br>Jetzt noch Gewschwindigkeit, Uhrzeit und Pausen anpassen.</p>
+<div class="title">
+<h1>Marschzeit&shy;tabelle<br />leicht gemacht.</h1>
+<p class="subtitle">Wir übertragen deine Route automatisch in eine Marschzeittabelle, damit du dich auf die wichtigen Teile der Planung konzentrieren kannst.</p>
+</div>
+<div class="tutorial">
+    <div>
+        <h2>1. Route planen</h2>
+        <p>Route einzeichnen, Wegpunkte setzen - das kennst du bereits.</p>
+        <h2>2. Exportieren und hochladen</h2>
+        <p>In wenigen Klicks erledigt.</p>
+        <h2>3. Voilà!</h2>
+        <p>Schon hast du deine Marschzeittabelle. Jetzt noch Gewschwindigkeit, Uhrzeit und Pausen anpassen.</p>
+    </div>
+    <img src="example.png" alt="Beispiel" />
 </div>
 
 <style>
-    div {
+    .title {
         text-align: center;
     }
     
@@ -29,6 +34,26 @@
     
     .subtitle {
         font-size: 1.4em;
+    }
+
+    .tutorial {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        gap: 1rem;
+        margin: 2rem;
+    }
+
+    .tutorial div {
+        flex: 1 1 15rem;
+    }
+
+    .tutorial img {
+        width: 100%;
+        flex: 0 1 20rem;
+        border-radius: 1rem;
+        box-shadow: 1rem 1rem 2rem rgba(0, 0, 0, 0.1);
     }
 
     h2 {
