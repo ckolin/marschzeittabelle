b7aec9e0e1f6fa56fd7403e925e8ee1737c4eeb5
ckolin <25738463+ckolin@users.noreply.github.com>
2022-01-20 11:52:42 +0100

    Show ascent and descent

--- src/Info.svelte
+++ src/Info.svelte
@@ -3,10 +3,26 @@
 
     export let route;
     export let speed;
+
+    $: ({ascent, descent} = calculateHeightDifference(route));
+
+    function calculateHeightDifference(route) {
+        let ascent = 0, descent = 0;
+        for (let i = 1; i < route.markers.length; i++) {
+            const diff = route.markerProfile[i].height - route.markerProfile[i - 1].height;
+            if (diff > 0) {
+                ascent += diff;
+            } else {
+                descent -= diff;
+            }
+        }
+        return { ascent, descent };
+    }
 </script>
 
 Start: <b>{route.markers[0].name}</b><br />
 Ende: <b>{route.markers[route.markers.length - 1].name}</b><br />
+Auf-/Abstieg: ↑ <b>{Math.round(ascent)} m</b> ↓ <b>{Math.round(descent)} m</b><br />
 Distanz: <b>{(route.distanceSum[route.line.length - 1] / 1000).toFixed(1)} km</b><br />
 Aufwand: <b>{route.effortSum[route.markers.length - 1].toFixed(1)} Lkm</b><br />
 Dauer: <b>{formatDuration(route.effortSum[route.markers.length - 1] / speed)} h</b><br />
