eafe5a642fa5b6a4b037d41c38efcf4621a36539
ckolin <25738463+ckolin@users.noreply.github.com>
2022-01-22 18:17:56 +0100

    Hide help links when printing

--- src/HelpLink.svelte
+++ src/HelpLink.svelte
@@ -2,7 +2,7 @@
     export let topic = null;
 </script>
 
-<a href={topic == null ? "hilfe" : `hilfe#${topic}`} target="_blank">?</a>
+<a href={topic == null ? "hilfe" : `hilfe#${topic}`} target="_blank" class="noprint">?</a>
 
 <style>
     a {
