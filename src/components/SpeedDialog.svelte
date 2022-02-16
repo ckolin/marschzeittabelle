19c3d78ca654bec59fabc1d0a5fd3445ae908e5e
ckolin <25738463+ckolin@users.noreply.github.com>
2022-02-16 17:44:15 +0100

    Swap buttons

--- src/components/SpeedDialog.svelte
+++ src/components/SpeedDialog.svelte
@@ -15,11 +15,11 @@
 <Dialog title="Geschwindigkeit anpassen" bind:show>
     <label for="speed">Geschwindigkeit (Lkm/h)</label>
     <input id="speed" type="number" min="0.1" step="0.1" required bind:value={input} />
-    <button type="button" class="pill" on:click={() => input += 0.5}>
-        <Icon name="add" /> Schneller
-    </button>
     <button type="button" class="pill" on:click={() => input = Math.max(0.5, input - 0.5)}>
         <Icon name="remove" /> Langsamer
+    </button>
+    <button type="button" class="pill" on:click={() => input += 0.5}>
+        <Icon name="add" /> Schneller
     </button>
     <br />
 </Dialog>
