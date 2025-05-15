3794a3fae2582545e627d69d09fe8ac959b64a79
ckolin <25738463+ckolin@users.noreply.github.com>
2025-05-15 13:34:59 +0200

    Rewrite help url

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,20 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [svelte()],
+    plugins: [
+        svelte(),
+        {
+            name: "help",
+            configureServer(server) {
+                server.middlewares.use((req, res, next) => {
+                    // @ts-ignore
+                    if (req.url === "/help") {
+                        // @ts-ignore
+                        req.url = "/help.html";
+                    }
+                    next();
+                });
+            },
+        },
+    ],
 });
