f2eb894e26fdc614532cb0f707deddcc4bddf276
ckolin <25738463+ckolin@users.noreply.github.com>
2025-05-20 18:03:11 +0200

    Change file extension to make cloudflare use compression

--- src/lib/routing.ts
+++ src/lib/routing.ts
@@ -307,7 +307,7 @@ export class Router {
 
     private async loadTile(id: string): Promise<FwdEdge[]> {
         const res = await fetch(
-            `${import.meta.env.VITE_TILE_URL}/${id}.msgpack`,
+            `${import.meta.env.VITE_TILE_URL}/${id}.txt`,
         );
         if (!res.ok) {
             // TODO: Could be nonexistent tile, could be something else
